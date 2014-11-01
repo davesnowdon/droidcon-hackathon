dc10af9192a731016e5f94010dacb629c5fd5a45
Dave Snowdon <dave@davesnowdon.com>
2014-11-01 18:11:02 +0000

    got heartrate sensor working

--- wear/src/main/java/com/emoware/emoware/WearHome.java
+++ wear/src/main/java/com/emoware/emoware/WearHome.java
@@ -1,24 +1,82 @@
 package com.emoware.emoware;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.widget.TextView;
 
-public class WearHome extends Activity {
+import java.util.concurrent.CountDownLatch;
+
+public class WearHome extends Activity implements SensorEventListener {
+
+    private static final String TAG = WearHome.class.getName();
+
+    private static final int SENSOR_TYPE_HEARTRATE = 65562;
 
     private TextView mTextView;
+
+    SensorManager mSensorManager;
+    private Sensor mHeartRateSensor;
+
+    private CountDownLatch latch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wear_home);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        latch = new CountDownLatch(1);
+
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                latch.countDown();
             }
         });
+
+        mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
+        //Sensor mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        mHeartRateSensor = mSensorManager.getDefaultSensor(SENSOR_TYPE_HEARTRATE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mSensorManager.registerListener(this, this.mHeartRateSensor, 3);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        try {
+            latch.await();
+            if(sensorEvent.values[0] > 0){
+                Log.d(TAG, "sensor event: " + sensorEvent.accuracy + " = " + sensorEvent.values[0]);
+                mTextView.setText(String.valueOf(sensorEvent.values[0]));
+                //accuracy.setText("Accuracy: "+sensorEvent.accuracy);
+                //sensorInformation.setText(sensorEvent.sensor.toString());
+            }
+
+        } catch (InterruptedException e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(TAG, "accuracy changed: " + accuracy);
     }
 }
