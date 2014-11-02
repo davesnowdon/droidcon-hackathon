d58402ffa95c73c7e0e4de68d1691591dfa59cd2
Jairo Diaz <jairo.diaz@codescrum.com>
2014-11-02 08:31:39 +0000

    Added fragment for person information

---
+++ mobile/src/main/java/com/emoware/emoware/FragmentOne.java
@@ -0,0 +1,21 @@
+package com.emoware.emoware;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jairodiaz on 02/11/2014.
+ */
+public class FragmentOne extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_one, container, false);
+        return v;
+    }
+}
--- mobile/src/main/java/com/emoware/emoware/Main.java
+++ mobile/src/main/java/com/emoware/emoware/Main.java
@@ -1,9 +1,15 @@
 package com.emoware.emoware;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 //Code for creating a dynamic list of users:
 // http://www.coderzheaven.com/2011/03/12/creating-scrolling-listview-in-android/
@@ -12,12 +18,27 @@ import android.view.MenuItem;
 // http://developer.android.com/guide/components/fragments.html
 
 public class Main extends Activity {
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.b1);
+
+        b1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                FragmentOne f1 = new FragmentOne();
+                ft.add(R.id.fr1_id, f1);
+                ft.addToBackStack("f1");
+                ft.commit();
+            }
+        });
     }
+
 
 
     @Override
