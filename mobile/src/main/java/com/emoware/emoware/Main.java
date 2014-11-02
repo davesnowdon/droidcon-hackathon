71b56a749a9b07e6d2969546fc7e7653ba0dfde4
Jairo Diaz <jairo.diaz@codescrum.com>
2014-11-02 09:17:16 +0000

    Configured Touch motion instead of button click

--- mobile/src/main/java/com/emoware/emoware/Main.java
+++ mobile/src/main/java/com/emoware/emoware/Main.java
@@ -7,9 +7,14 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.view.View.OnTouchListener;
 
 //Code for creating a dynamic list of users:
 // http://www.coderzheaven.com/2011/03/12/creating-scrolling-listview-in-android/
@@ -18,23 +23,58 @@ import android.view.View.OnClickListener;
 // http://developer.android.com/guide/components/fragments.html
 
 public class Main extends Activity {
-    Button b1;
+//    Button b1;
+    TextView t1;
+    LinearLayout l1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1 = (Button) findViewById(R.id.b1);
+//        b1 = (Button) findViewById(R.id.b1);
+//
+//        b1.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FragmentManager fm = getFragmentManager();
+//                FragmentTransaction ft = fm.beginTransaction();
+//                FragmentOne f1 = new FragmentOne();
+//                ft.add(R.id.fr1_id, f1);
+//                ft.addToBackStack("f1");
+//                ft.commit();
+//            }
+//        });
 
-        b1.setOnClickListener(new OnClickListener() {
+        l1 = (LinearLayout) findViewById(R.id.l1);
+
+        l1.setOnTouchListener(new OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                FragmentManager fm = getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                FragmentOne f1 = new FragmentOne();
-                ft.add(R.id.fr1_id, f1);
-                ft.addToBackStack("f1");
-                ft.commit();
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+
+        t1 = (TextView) findViewById(R.id.t1);
+
+        t1.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                    case MotionEvent.ACTION_POINTER_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        FragmentManager fm = getFragmentManager();
+                        FragmentTransaction ft = fm.beginTransaction();
+                        FragmentOne f1 = new FragmentOne();
+                        ft.add(R.id.fr1_id, f1);
+                        ft.addToBackStack("f1");
+                        ft.commit();
+                        return true;
+                    default:
+                        return false;
+
+                }
             }
         });
     }
