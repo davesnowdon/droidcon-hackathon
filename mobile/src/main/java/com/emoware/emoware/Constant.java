13a8c55be9a2c31fd405edf8adcbc58e1e3a9316
Dave Snowdon <dave@davesnowdon.com>
2014-11-02 13:56:00 +0000

    last version before demo - data api code commented out

---
+++ mobile/src/main/java/com/emoware/emoware/Constant.java
@@ -0,0 +1,7 @@
+package com.emoware.emoware;
+
+public class Constant {
+    public static final String LANDING_SITE_IMAGE_RESPONSE = "imageResponse";
+    public static final String KEY_LANDING_SITE_IMAGE = "personImage";
+    public static final String KEY_TIMESTAMP = "timestamp";
+}
--- mobile/src/main/java/com/emoware/emoware/Main.java
+++ mobile/src/main/java/com/emoware/emoware/Main.java
@@ -1,9 +1,13 @@
 package com.emoware.emoware;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.IntentSender;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Looper;
@@ -25,7 +29,17 @@ import com.aldebaran.qimessaging.helpers.al.ALMemory;
 import com.aldebaran.qimessaging.helpers.al.ALMotion;
 import com.aldebaran.qimessaging.helpers.al.ALTextToSpeech;
 import com.aldebaran.qimessaging.helpers.al.ALVideoDevice;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Date;
@@ -49,6 +63,15 @@ public class Main extends Activity {
     private ALMemory memoryProxy;
     private ALTextToSpeech ttsProxy;
 
+    GoogleApiClient dataClient;
+
+    // Request code to use when launching the resolution activity
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    // Unique tag for the error dialog fragment
+    private static final String DIALOG_ERROR = "dialog_error";
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
+
     ListView list;
     private List<String> List_of_persons;
 
@@ -62,7 +85,26 @@ public class Main extends Activity {
         List_of_persons = new ArrayList<String>();
         list = (ListView)findViewById(R.id.listview);
         CreateListView();
+
+        // TODO!
+        //dataClient = getGoogleApiClient(this);
     }
+
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingError) {  // more about this later
+            dataClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        dataClient.disconnect();
+        super.onStop();
+    }
+    */
 
     public void onConnectNao(View view) {
         Log.i(TAG, "Connect clicked");
@@ -111,7 +153,7 @@ public class Main extends Activity {
                         moduleName = video.subscribeCamera("demoAndroid", topCamera, resolution, colorspace, frameRate);
                         if (moduleName != null) {
                         myAlmemory.call("raiseMicroEvent", "AndroidIP", "finished video.subscribeCamera:" + moduleName);
-                        final Bitmap MyImage = getVideo();
+                        final Bitmap bitmap = getVideo();
                         video.unsubscribe(moduleName);
 
                         try{
@@ -121,10 +163,13 @@ public class Main extends Activity {
                                 public void run() {
 
                                     image = (ImageView) findViewById(R.id.imageView);
-                                    image.setImageBitmap(MyImage);
+                                    image.setImageBitmap(bitmap);
                                 }
                             });
 
+                            // send the bitmap data to the wearable
+                            // TODO!
+                            //sendBitmapToWearable(bitmap);
 
                             myAlmemory.call("raiseMicroEvent","AndroidIP", "imageView update complete");
                         } catch (Exception e) {
@@ -163,7 +208,108 @@ public class Main extends Activity {
 //        return null;
     }
 
+    /*
+    private GoogleApiClient getGoogleApiClient(Context context) {
+        return new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle connectionHint) {
+                        Log.d(TAG, "onConnected: " + connectionHint);
+                        // Now you can use the data layer API
+                    }
+                    @Override
+                    public void onConnectionSuspended(int cause) {
+                        Log.d(TAG, "onConnectionSuspended: " + cause);
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult result) {
+                        Log.d(TAG, "onConnectionFailed: " + result);
 
+                        if (mResolvingError) {
+                            // Already attempting to resolve an error.
+                            return;
+                        } else if (result.hasResolution()) {
+                            try {
+                                mResolvingError = true;
+                                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+                            } catch (IntentSender.SendIntentException e) {
+                                // There was an error with the resolution intent. Try again.
+                                dataClient.connect();
+                            }
+                        } else {
+                            // Show dialog using GooglePlayServicesUtil.getErrorDialog()
+                            showErrorDialog(result.getErrorCode());
+                            mResolvingError = true;
+                        }
+                    }
+                })
+                .addApi(Wearable.API)
+                .build();
+    }
+
+    public void sendBitmapToWearable(Bitmap bitmap) {
+        if(null != bitmap) {
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+
+            Log.d(TAG, "requestLandingSiteImage loaded. Bitmap size: " +
+                    bitmap.getWidth() + " x " + bitmap.getHeight());
+
+            Asset landingSiteAsset = Asset.createFromBytes(stream.toByteArray());
+
+            PutDataMapRequest dataMap =
+                    PutDataMapRequest.create(Constant.LANDING_SITE_IMAGE_RESPONSE);
+            dataMap.getDataMap().putAsset(Constant.KEY_LANDING_SITE_IMAGE,
+                    landingSiteAsset);
+            dataMap.getDataMap().putLong(Constant.KEY_TIMESTAMP, new Date().getTime());
+
+            PutDataRequest request = dataMap.asPutDataRequest();
+            PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi
+                    .putDataItem(dataClient, request);
+            DataApi.DataItemResult result = pendingResult.await();
+            Log.d(TAG, "requestLandingSiteImage. Bitmap asset set: " +
+                    result.getDataItem().getUri());
+        } else {
+            Log.d(TAG, "Loading bitmap failed.");
+        }
+    }
+
+    // Creates a dialog for an error message
+    private void showErrorDialog(int errorCode) {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getSupportFragmentManager(), "errordialog");
+    }
+
+    // Called from ErrorDialogFragment when the dialog is dismissed.
+    public void onDialogDismissed() {
+        mResolvingError = false;
+    }
+
+    // A fragment to display an error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+        public ErrorDialogFragment() { }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GooglePlayServicesUtil.getErrorDialog(errorCode,
+                    this.getActivity(), REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            ((Main)getActivity()).onDialogDismissed();
+        }
+    }
+    */
 
     boolean isNotBlank(String str) {
         return (null != str) && !str.trim().equals("");
